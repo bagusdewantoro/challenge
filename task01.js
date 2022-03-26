3f4b3f60cf1a3b0d317bcdbd72a04a143000ae64
bagusdewantoro <bagusdewantoro86@gmail.com>
2022-03-26 10:46:58 +0700

    Revised. Previous code was wrong

--- task01.js
+++ task01.js
@@ -1,29 +1,19 @@
 // function
 function findLongestWord(sentence) {
-  const words = sentence.toLowerCase().match(/[a-z]+/gi);
-  let char = 0;
-  let longest = [];
-  for (word of words) {
-    if (word.length >= char) {
-      char = word.length
-      longest.push(word)
-    }
-    longestWord = word.length;
-  }
-  let vowelCount = 0;
-  let longestVowel;
-  for (word of longest) {
-    let vowel = word.match(/[aeiou]+/gi).join('')
-    if (vowel.length >= vowelCount) {
-      vowelCount = vowel.length;
-      longestVowel = word;
-    }
-  }
-  return longestVowel;
+  // get array of the longest words
+  const regex1 = /[a-z]/gi;
+  const ar = sentence.toLowerCase().split(' ').sort((x, y) => y.length - x.length).map(w => w.match(regex1).join(''));
+  const maxWord = ar.filter(w => w.length === ar[0].length);
+
+  // get highest number of vowels
+  const regex2 = /[aiueo]/g;
+  return maxWord.sort((x, y) => y.match(regex2).length - x.match(regex2).length)[0];
 }
 
 // sentence example
-const string1 = "Smart people learn from everything and everyone, average people from their experience, stupid people already, have all the answers"
+const string1 = "Smart people learn from everything and everyone, average people from their experience, stupid people already, have all the answers";
+const string2 = "bAiEoc, bcdfgAAhc, bcdfgAAac";
 
 // find longest word with highest number of vowels
 console.log(findLongestWord(string1)); // experience
+console.log(findLongestWord(string2)); // bcdfgaaac
